8878a32710990d415831bc167f17b3999b7833c4
ferreiraryan <ryck342020@hotmail.com>
2025-09-29 18:40:32 -0300

    Adciona: o middleware as rotas de authenticacao

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { getProfile, loginUser, registerUser } from '../controllers/authController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { validate } from '../middlewares/validateMiddleware';
+import { loginSchema, registerSchema } from '../schemas/authSchemas';
 
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', validate(registerSchema), registerUser);
+router.post('/login', validate(loginSchema), loginUser);
+
 router.get('/profile', authMiddleware, getProfile);
 
 export default router;
