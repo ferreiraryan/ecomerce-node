149a628923eae9a25b4a40d58a7a98aeb98e79d0
ferreiraryan <Ryck342020@hotmail.com>
2025-10-05 13:29:05 -0300

    Feat: Tela de login usando componente padronizado Input

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Input } from '../components/common/Input'
 
 export function LoginPage() {
   const [email, setEmail] = useState('');
@@ -22,18 +23,28 @@ export function LoginPage() {
       <div className='bg-cyan-200 rounded-2xl p-8 w-full max-w-md shadow-lg'>
         <h1 className='text-2xl font-bold text-center text-gray-800 mb-6'>Login:</h1>
         <form onSubmit={handleSubmit} className='space-y-6'>
-          <div>
-            <label htmlFor="email" className='block text-sm font-medium text-gray-700'>
-              Endereço de email:
-            </label>
-            <input type="email" id='email' name='email' required autoComplete='email' value={email} onChange={(e) => setEmail(e.target.value)} className='bg-fuchsia-50 mt-1 block rounded-lg font-light text-sm shadow-lg w-full px-3 py-2 border border-gray-200 focus:outline-none focus:ring-blue-300 focus:border-blue-500' />
-          </div>
-          <div>
-            <label htmlFor="password" className='block text-sm font-medium text-gray-700'>
-              Senha:
-            </label>
-            <input type="password" id='password' name='password' required autoComplete='password' value={password} onChange={(e) => setPassword(e.target.value)} className='bg-fuchsia-50 mt-1 mb-3 block rounded-lg font-light text-sm shadow-lg w-full px-3 py-2 border border-gray-200 focus:outline-none focus:ring-blue-300 focus:border-blue-500' />
-          </div>
+          <Input
+            label="Endereço de Email"
+            id="email"
+            type="email"
+            name="email"
+            required
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Input
+            label="Senha"
+            id="password"
+            type="password"
+            name="password"
+            required
+            autoComplete="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           <div>
             <button type='submit' className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
