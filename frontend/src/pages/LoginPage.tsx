99670ca07307ab2f0f204a1f6f55fa91d8b1378d
ferreiraryan <Ryck342020@hotmail.com>
2025-10-05 13:11:23 -0300

    Finalizado: o front da pagina de login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ export function LoginPage() {
     <main className='bg-fuchsia-50 min-h-screen flex items-center justify-center'>
       <div className='bg-cyan-200 rounded-2xl p-8 w-full max-w-md shadow-lg'>
         <h1 className='text-2xl font-bold text-center text-gray-800 mb-6'>Login:</h1>
-        <form action="">
+        <form onSubmit={handleSubmit} className='space-y-6'>
           <div>
             <label htmlFor="email" className='block text-sm font-medium text-gray-700'>
               Endereço de email:
@@ -32,7 +32,7 @@ export function LoginPage() {
             <label htmlFor="password" className='block text-sm font-medium text-gray-700'>
               Senha:
             </label>
-            <input type="password" id='password' name='password' required autoComplete='email' value={email} onChange={(e) => setEmail(e.target.value)} className='bg-fuchsia-50 mt-1 block rounded-lg font-light text-sm shadow-lg w-full px-3 py-2 border border-gray-200 focus:outline-none focus:ring-blue-300 focus:border-blue-500' />
+            <input type="password" id='password' name='password' required autoComplete='password' value={password} onChange={(e) => setPassword(e.target.value)} className='bg-fuchsia-50 mt-1 mb-3 block rounded-lg font-light text-sm shadow-lg w-full px-3 py-2 border border-gray-200 focus:outline-none focus:ring-blue-300 focus:border-blue-500' />
           </div>
           <div>
             <button type='submit' className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
